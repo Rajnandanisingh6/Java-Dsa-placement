e1a28d0f8bb94a7b6423984918b6bf5db6c21d1e
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-09-13 13:59:12 +0530

    Learn variables and data types

---
+++ JAVA_DAY1/Ques.java
@@ -0,0 +1,17 @@
+public class Ques {
+    public static void main(String[] args) {
+        
+
+        
+// Homework Problems
+// Try to declare meaningful variables of each type. Eg - a variable named age should be a numeric type (int or float) not byte.
+
+// Make a program that takes the radius of a circle as input, calculates its radius and area and prints it as output to the user.
+// Make a program that prints the table of a number that is input by the user. 
+// (HINT - You will have to write 10 lines for this but as we proceed in the course you will be studying about ‘LOOPS’ that will simplify your work A LOT!)
+
+
+
+    }
+    
+}
---
+++ SecondClass.java
@@ -0,0 +1,47 @@
+import java .util.*;
+public class SecondClass {
+    public static void main(String[] args) {
+        //Output
+        /* System.out.println("Hello duniya");
+        System.out.println("Hello duniya");
+        System.out.print("Hello duniya \n");
+        System.out.print("Hello duniya");
+         */
+        //output
+        //  System.out.println("*");
+        //  System.out.println("**");
+        //  System.out.println("***");
+        //  System.out.println("****");
+        //  System.out.println("*****");
+
+        //Variables
+        // int a=10;
+        // int b=30;
+        // int sum=a+b;
+        // System.out.println(sum);
+
+        // int mul = a*b;
+        // System.out.println(mul);
+
+        //Input
+        // Scanner sc= new Scanner(System.in);
+        // String name=sc.next();
+        // String nam1e=sc.nextLine();
+        //nextInt
+        //nextFloat
+        // System.out.println(name);
+
+        //ques1 -- take 2 variables a and b and print their sum
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a +b;
+        
+        System.out.println(sum);
+
+
+        
+
+    }
+    
+}
