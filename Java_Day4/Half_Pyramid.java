65d0893bc830a70c273a18d255c7065d707302a7
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-09-18 19:45:05 +0530

    half_pyramid_pattern

---
+++ Java_Day4/Half_Pyramid.java
@@ -0,0 +1,18 @@
+package Java_Day4;
+
+public class Half_Pyramid {
+    public static void main(String[] args) {
+        //Half Pyramid
+        int n=4;
+        //outer
+        for(int i=1;i<=n;i++){
+            //inner loop
+            for(int j=1;j<=i;j++){
+               System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    
+}
